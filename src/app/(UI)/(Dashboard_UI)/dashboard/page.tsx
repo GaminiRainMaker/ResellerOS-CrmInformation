5c287918c7f122046eae101b5d1bbed95913fac9
GaminiRainMaker <118426903+GaminiRainMaker@users.noreply.github.com>
2023-12-04 17:26:19 +0530

    Update page.tsx

--- src/app/(UI)/(Dashboard_UI)/dashboard/page.tsx
+++ src/app/(UI)/(Dashboard_UI)/dashboard/page.tsx
@@ -2,25 +2,21 @@
 
 import React from 'react';
 
-import SideBar from '../layouts/SideBar';
-
 // import {CustomDiv2} from '../layouts/styled-components';
 
 const Dashboard = () => {
   console.log('erer');
   return (
-    <div>
-      <div
-        style={{
-          display: 'inline-flex',
-          padding: '43px 48px',
-          flex: 'column',
-          //    align-items: "flex-start";
-          gap: '36px',
-        }}
-      >
-        Dashboard
-      </div>
+    <div
+      style={{
+        display: 'inline-flex',
+        padding: '43px 48px',
+        flex: 'column',
+        //    align-items: "flex-start";
+        gap: '36px',
+      }}
+    >
+      Dashboard
     </div>
   );
 };
