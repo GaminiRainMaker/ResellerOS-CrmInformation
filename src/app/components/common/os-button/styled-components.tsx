ff01e12cb4f545676f446c8ef4de7279a37a468b
GaminiRainMaker <118426903+GaminiRainMaker@users.noreply.github.com>
2023-12-06 17:23:07 +0530

    Added Button styli ng

--- src/app/components/common/os-button/styled-components.tsx
+++ src/app/components/common/os-button/styled-components.tsx
@@ -19,9 +19,39 @@ export const ButtonStyled = styled(Button)<{buttontype?: any}>`
     if (props.buttontype === 'PRIMARY_ICON') {
       return '#1c3557 !important';
     }
+    if (props.buttontype === 'PRIMARY_DISABLED') {
+      return '#C7CDD5 !important';
+    }
+    if (props.buttontype === 'SECONDARY') {
+      return 'transperent !important';
+    }
+    if (props.buttontype === 'SECONDARY_DISABLED') {
+      return 'transperent !important';
+    }
     return '';
   }};
-  border: 1px solid #14263e !important;
+  // border: 1px solid #14263e !important;
+  border: ${(props) => {
+    if (props.buttontype === 'PRIMARY') {
+      return '1px solid #14263e !important';
+    }
+    if (props.buttontype === 'PRIMARY_SMALL') {
+      return '1px solid #14263e !important';
+    }
+    if (props.buttontype === 'PRIMARY_ICON') {
+      return '1px solid #14263e !importantt';
+    }
+    if (props.buttontype === 'PRIMARY_DISABLED') {
+      return 'transparent !important';
+    }
+    if (props.buttontype === 'SECONDARY') {
+      return '1px solid #14263E !important';
+    }
+    if (props.buttontype === 'SECONDARY_DISABLED') {
+      return '1px solid #C6CDD5 !important';
+    }
+    return '#14263E !important';
+  }};
   color: ${(props) => {
     if (props.buttontype === 'PRIMARY') {
       return 'white !important';
@@ -32,8 +62,18 @@ export const ButtonStyled = styled(Button)<{buttontype?: any}>`
     if (props.buttontype === 'PRIMARY_ICON') {
       return 'white !important';
     }
+    if (props.buttontype === 'PRIMARY_DISABLED') {
+      return 'white !important';
+    }
+    if (props.buttontype === 'SECONDARY') {
+      return '#14263E !important';
+    }
+    if (props.buttontype === 'SECONDARY_DISABLED') {
+      return '#C6CDD5!important';
+    }
     return '#14263E !important';
   }};
+
   padding: ${(props) => {
     if (props.buttontype === 'PRIMARY') {
       return '12px 24px';
