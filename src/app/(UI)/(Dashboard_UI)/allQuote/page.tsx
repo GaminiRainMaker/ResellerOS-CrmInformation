c819edbec1e40e57e48fe9fedb423cc18c9eb5e3
GaminiRainMaker <118426903+GaminiRainMaker@users.noreply.github.com>
2023-12-08 11:57:37 +0530

    Update page.tsx

--- src/app/(UI)/(Dashboard_UI)/allQuote/page.tsx
+++ src/app/(UI)/(Dashboard_UI)/allQuote/page.tsx
@@ -24,11 +24,253 @@ import OsTabs from '@/app/components/common/os-tabs';
 import TabPane from 'antd/es/tabs/TabPane';
 import {useState} from 'react';
 import CommonDatePicker from '@/app/components/common/os-date-picker';
+import OsTable from '@/app/components/common/os-table';
 
 const AllQuote: React.FC = () => {
   const [token] = useThemeToken();
   const [activeTab, setActiveTab] = useState<any>('1');
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
 
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const Quotecolumns = [
+    {
+      title: 'Cage Code',
+      dataIndex: 'cage_code',
+      key: 'cage_code',
+    },
+    {
+      title: 'Credit Cards',
+      dataIndex: 'credit_cards',
+      key: 'credit_cards',
+    },
+    {
+      title: 'Customer Address',
+      dataIndex: 'customer_address',
+      key: 'customer_address',
+    },
+    {
+      title: 'Customer City',
+      dataIndex: 'customer_city',
+      key: 'customer_city',
+    },
+    {
+      title: 'Customer Contact',
+      dataIndex: 'customer_contact',
+      key: 'customer_contact',
+    },
+    {
+      title: 'Customer Email',
+      dataIndex: 'customer_email',
+      key: 'customer_email',
+    },
+    {
+      title: 'Customer Name',
+      dataIndex: 'customer_name',
+      key: 'customer_name',
+    },
+    {
+      title: 'Customer Phone',
+      dataIndex: 'customer_phone',
+      key: 'customer_phone',
+    },
+    {
+      title: 'Customer State',
+      dataIndex: 'customer_state',
+      key: 'customer_state',
+    },
+    {
+      title: 'Total Price',
+      dataIndex: 'customer_street',
+      key: 'customer_street',
+    },
+    {
+      title: 'Customer Zip',
+      dataIndex: 'customer_zip',
+      key: 'customer_zip',
+    },
+
+    {
+      title: 'Deal Id',
+      dataIndex: 'deal_id',
+      key: 'deal_id',
+    },
+    {
+      title: 'Distributor Address',
+      dataIndex: 'distributor_address',
+      key: 'distributor_address',
+    },
+    {
+      title: 'Distributor City',
+      dataIndex: 'distributor_city',
+      key: 'distributor_city',
+    },
+    {
+      title: 'Distributor Contact',
+      dataIndex: 'distributor_contact',
+      key: 'distributor_contact',
+    },
+    {
+      title: 'Distributor Email',
+      dataIndex: 'distributor_email',
+      key: 'distributor_email',
+    },
+    {
+      title: 'Distributor Fax',
+      dataIndex: 'distributor_fax',
+      key: 'distributor_fax',
+    },
+
+    {
+      title: 'Distributor Name',
+      dataIndex: 'distributor_name',
+      key: 'distributor_name',
+    },
+    {
+      title: 'Distributor Phone',
+      dataIndex: 'distributor_phone',
+      key: 'distributor_phone',
+    },
+    {
+      title: 'Distributor State',
+      dataIndex: 'distributor_state',
+      key: 'distributor_state',
+    },
+    {
+      title: 'Distributor Street',
+      dataIndex: 'distributor_street',
+      key: 'distributor_street',
+    },
+    {
+      title: 'Distributor Zip',
+      dataIndex: 'distributor_zip',
+      key: 'distributor_zip',
+    },
+
+    {
+      title: 'Duns Number',
+      dataIndex: 'duns_number',
+      key: 'duns_number',
+    },
+    {
+      title: 'Expiration Date',
+      dataIndex: 'expiration_date',
+      key: 'expiration_date',
+    },
+    {
+      title: 'Fob Shipping',
+      dataIndex: 'fob_shipping',
+      key: 'fob_shipping',
+    },
+    {
+      title: 'Ftin',
+      dataIndex: 'ftin',
+      key: 'ftin',
+    },
+
+    {
+      title: 'Oem Name',
+      dataIndex: 'oem_name',
+      key: 'oem_name',
+    },
+    {
+      title: 'Payment Terms',
+      dataIndex: 'payment_terms',
+      key: 'payment_terms',
+    },
+    {
+      title: 'Quote Amount',
+      dataIndex: 'quote_amount',
+      key: 'quote_amount',
+    },
+    {
+      title: 'Quote Date',
+      dataIndex: 'quote_date',
+      key: 'quote_date',
+    },
+    {
+      title: 'Quote Number',
+      dataIndex: 'quote_number',
+      key: 'quote_number',
+    },
+    {
+      title: 'Remit To',
+      dataIndex: 'remit_to',
+      key: 'remit_to',
+    },
+    {
+      title: 'Reseller Address',
+      dataIndex: 'reseller_address',
+      key: 'reseller_address',
+    },
+    {
+      title: 'Reseller City',
+      dataIndex: 'reseller_city',
+      key: 'reseller_city',
+    },
+    {
+      title: 'Reseller Contact',
+      dataIndex: 'reseller_contact',
+      key: 'reseller_contact',
+    },
+    {
+      title: 'Reseller Email',
+      dataIndex: 'reseller_email',
+      key: 'reseller_email',
+    },
+    {
+      title: 'Reseller Name',
+      dataIndex: 'reseller_name',
+      key: 'reseller_name',
+    },
+    {
+      title: 'Reseller Phone',
+      dataIndex: 'reseller_phone',
+      key: 'reseller_phone',
+    },
+    {
+      title: 'Reseller State',
+      dataIndex: 'reseller_state',
+      key: 'reseller_state',
+    },
+    {
+      title: 'Reseller Street',
+      dataIndex: 'reseller_street',
+      key: 'reseller_street',
+    },
+    {
+      title: 'Reseller Zip',
+      dataIndex: 'reseller_zip',
+      key: 'reseller_zip',
+    },
+    {
+      title: 'Shipping',
+      dataIndex: 'shipping',
+      key: 'shipping',
+    },
+    {
+      title: 'Shipping Amount',
+      dataIndex: 'shipping_amount',
+      key: 'shipping_amount',
+    },
+    {
+      title: 'Subtotal',
+      dataIndex: 'subtotal',
+      key: 'subtotal',
+    },
+    {
+      title: 'UEI',
+      dataIndex: 'uei',
+      key: 'uei',
+    },
+  ];
   const analyticsData = [
     {
       key: 1,
@@ -192,11 +434,25 @@ const AllQuote: React.FC = () => {
                     }
                   >
                     {item?.name}
+                    <div
+                      style={{
+                        // eslint-disable-next-line eqeqeq
+                        borderBottom:
+                          // eslint-disable-next-line eqeqeq
+                          activeTab == item?.key ? '2px solid #1C3557' : '',
+                        marginTop: '3px',
+                      }}
+                    />
                   </Typography>
                 }
                 key={item?.key}
               >
-                Hlo
+                <OsTable
+                  rowSelection={rowSelection}
+                  columns={Quotecolumns}
+                  dataSource={[]}
+                  scroll
+                />
               </TabPane>
             ))}
           </OsTabs>
