c3701eaab577273a7d5a8f0ae3f37d41fadae5f2
GaminiRainMaker <118426903+GaminiRainMaker@users.noreply.github.com>
2024-01-30 18:02:09 +0530

    Update client.ts

--- axios/client.ts
+++ axios/client.ts
@@ -3,8 +3,8 @@ import {requestInterceptor} from './interceptors/request.interceptor';
 import {responseInterceptor} from './interceptors/response.interceptor';
 
 const client = axios.create({
-  baseURL: 'http://localhost:4000',
-  // baseURL: 'http://ec2-3-218-195-191.compute-1.amazonaws.com:4000',
+  // baseURL: 'http://localhost:4000',
+  baseURL: 'http://ec2-3-218-195-191.compute-1.amazonaws.com:4000',
   // baseURL: 'https://api.reselleros.com',
   // baseURL: 'http://ec2-3-218-195-191.compute-1.amazonaws.com:4000',
   withCredentials: false,
