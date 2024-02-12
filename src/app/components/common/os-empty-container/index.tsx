bb9732d13501ff3667ddeac530c2f2238c4de0d5
GaminiRainMaker <118426903+GaminiRainMaker@users.noreply.github.com>
2024-02-12 12:16:17 +0530

    added build fixes

--- src/app/components/common/os-empty-container/index.tsx
+++ src/app/components/common/os-empty-container/index.tsx
@@ -43,7 +43,7 @@ const EmptyContainer: FC<EmptyContainerInterface> = ({
           text={actionButton}
           buttontype="PRIMARY"
           icon={<PlusIcon />}
-          clickHandler={() => onClick()}
+          clickHandler={() => onClick}
         />
       )}
     </Space>
--- src/app/components/common/os-opportunity-analytic-card/os-opportunity-analytic-card-interface.ts
+++ src/app/components/common/os-opportunity-analytic-card/os-opportunity-analytic-card-interface.ts
@@ -1,3 +1,3 @@
 export interface OsOpportunityAnalyicCardInterface {
-    OpportunityData: any;
+  OpportunityData: any;
 }
