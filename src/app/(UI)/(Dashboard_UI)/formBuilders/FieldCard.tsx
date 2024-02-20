c01bc0c36bbdb76dce551b8454b4d0b681b0d21c
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2024-02-20 15:45:34 +0530

    create new UI for form Builder

---
+++ src/app/(UI)/(Dashboard_UI)/formBuilders/FieldCard.tsx
@@ -0,0 +1,170 @@
+import {Space} from '@/app/components/common/antd/Space';
+import useThemeToken from '@/app/components/common/hooks/useThemeToken';
+import OsButton from '@/app/components/common/os-button';
+import Typography from '@/app/components/common/typography';
+import {
+  ArrowDownIcon,
+  CalendarDaysIcon,
+  CheckCircleIcon,
+  ClipboardDocumentCheckIcon,
+  ClockIcon,
+  CurrencyDollarIcon,
+  DocumentDuplicateIcon,
+  DocumentTextIcon,
+  EllipsisHorizontalCircleIcon,
+  EnvelopeIcon,
+  MinusIcon,
+  PhoneIcon,
+  RadioIcon,
+  TableCellsIcon,
+} from '@heroicons/react/24/outline';
+import FiledsDraggable from '../formbuilder/FiledsDraggable';
+
+const FieldCard = () => {
+  const [token] = useThemeToken();
+
+  const cardFields = [
+    {
+      key: 1,
+      title: 'Text Fields',
+      childitem: [
+        {
+          key: 1,
+          name: 'Text Content',
+          icon: <DocumentTextIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 2,
+          name: 'Add Section',
+          icon: <DocumentDuplicateIcon width={15} color={token?.colorInfo} />,
+        },
+      ],
+    },
+    {
+      key: 2,
+      title: 'Fields',
+      childitem: [
+        {
+          key: 3,
+          name: 'Text',
+          icon: <DocumentTextIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 4,
+          name: 'Multi-Select',
+          icon: (
+            <ClipboardDocumentCheckIcon width={15} color={token?.colorInfo} />
+          ),
+        },
+        {
+          key: 5,
+          name: 'Drop Down',
+          icon: <ArrowDownIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 6,
+          name: 'Date',
+          icon: <CalendarDaysIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 7,
+          name: 'Time',
+          icon: <ClockIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 8,
+          name: 'Currency',
+          icon: <CurrencyDollarIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 9,
+          name: 'Contact',
+          icon: <PhoneIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 10,
+          name: 'Email',
+          icon: <EnvelopeIcon width={15} color={token?.colorInfo} />,
+        },
+      ],
+    },
+    {
+      key: 3,
+      title: 'Sections',
+      childitem: [
+        {
+          key: 11,
+          name: 'Table',
+          icon: <TableCellsIcon width={15} color={token?.colorInfo} />,
+        },
+        {key: 12, name: 'Attachment', icon: ''},
+      ],
+    },
+    {
+      key: 4,
+      title: 'Elements',
+      childitem: [
+        {
+          key: 13,
+          name: 'Checkbox',
+          icon: <CheckCircleIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 14,
+          name: 'Radio Button',
+          icon: (
+            <EllipsisHorizontalCircleIcon width={15} color={token?.colorInfo} />
+          ),
+        },
+        {
+          key: 15,
+          name: 'Toggle',
+          icon: <RadioIcon width={15} color={token?.colorInfo} />,
+        },
+        {
+          key: 16,
+          name: 'Line Break',
+          icon: <MinusIcon width={15} color={token?.colorInfo} />,
+        },
+      ],
+    },
+  ];
+  return (
+    <>
+      <div>
+        {cardFields?.map((itemtab: any) => (
+          <Space direction="vertical" key={itemtab?.key}>
+            <div
+              style={{
+                marginTop: itemtab?.key === 1 ? '' : '24px',
+              }}
+            >
+              <Typography name="Body 4/Medium" color={token?.colorPrimaryText}>
+                {itemtab?.title}
+              </Typography>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: '8px',
+              }}
+            >
+              {itemtab?.childitem?.map((itemild: any) => (
+                <FiledsDraggable key="Attachment">
+                  <OsButton
+                    buttontype="BUILD_BUTTON"
+                    text={itemild?.name}
+                    icon={itemild?.icon}
+                  />
+                </FiledsDraggable>
+              ))}
+            </div>
+          </Space>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default FieldCard;
--- src/app/(UI)/(Dashboard_UI)/layout.tsx
+++ src/app/(UI)/(Dashboard_UI)/layout.tsx
@@ -2,6 +2,7 @@
 
 import {Layout} from 'antd';
 import React from 'react';
+import {usePathname} from 'next/navigation';
 import CustomHeader from './layouts/Header';
 import SideBar from './layouts/SideBar';
 
@@ -11,13 +12,18 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   return (
     <>
       <Layout style={{minHeight: '95vh'}}>
         <CustomHeader />
         <Layout>
           <SideBar />
-          <Content>{children}</Content>
+          <Content
+            style={{padding: pathname === '/formBuilders' ? '0px' : '24px'}}
+          >
+            {children}
+          </Content>
         </Layout>
       </Layout>
     </>
