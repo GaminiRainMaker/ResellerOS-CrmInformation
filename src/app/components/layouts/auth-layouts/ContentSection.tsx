c3da5be90a76aa25b6a863bb6fbcba6d7bb89b36
GaminiRainMaker <118426903+GaminiRainMaker@users.noreply.github.com>
2024-02-21 12:54:23 +0530

    Update ContentSection.tsx

--- src/app/components/layouts/auth-layouts/ContentSection.tsx
+++ src/app/components/layouts/auth-layouts/ContentSection.tsx
@@ -357,9 +357,9 @@ const ContentSection: FC<AuthLayoutInterface> = ({
                     {' '}
                     Register Now
                   </Typography>
-                  {/* <button onClick={() => router?.push('/formBuilder')}>
+                  <button onClick={() => router?.push('/formBuilder')}>
                     ddd
-                  </button> */}
+                  </button>
                 </Typography>
               )}
             </Space>
