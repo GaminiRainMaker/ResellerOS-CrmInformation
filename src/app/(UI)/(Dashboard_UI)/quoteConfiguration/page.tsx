aee5660468e01cfafe65de4f2bafd8d462acf4e3
Gamini Jindal <gjindal@rainmakercloud.com>
2024-03-14 15:46:31 +0530

    superadmin quote configuration changes done

--- src/app/(UI)/(Dashboard_UI)/quoteConfiguration/page.tsx
+++ src/app/(UI)/(Dashboard_UI)/quoteConfiguration/page.tsx
@@ -20,6 +20,7 @@ import {
   queryQuoteConfiguration,
 } from '../../../../../redux/actions/quoteConfiguration';
 import {useAppDispatch, useAppSelector} from '../../../../../redux/hook';
+import getColumns from './tableColumns';
 
 const AllQuote: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -42,152 +43,37 @@ const AllQuote: React.FC = () => {
   }, [quoteConfigData]);
 
   const handleButtonClick = () => {
-    quoteConfig?.forEach((dataItem: any) =>
-      dispatch(insertQuoteConfiguration(dataItem)).then((d) => {
-        if (d?.payload) {
-          dispatch(queryQuoteConfiguration({}));
-        }
-      }),
-    );
+    const finalArr: any = [];
+    let indexError = -1;
+    for (let i = 0; i < quoteConfig.length; i++) {
+      const dataItem: any = quoteConfig[i];
+      const index = finalArr.findIndex(
+        (item: any) =>
+          item.distributor_id === dataItem.distributor_id &&
+          item.oem_id === dataItem.oem_id,
+      );
+      if (index === -1) {
+        finalArr.push(dataItem);
+      } else {
+        indexError = index;
+      }
+    }
+    if (indexError === -1) {
+      finalArr?.forEach((dataItem: any) => {
+        dispatch(insertQuoteConfiguration(dataItem)).then((d) => {
+          if (d?.payload) {
+            dispatch(queryQuoteConfiguration({}));
+          }
+        });
+      });
+    } else {
+      const arr: any = [...quoteConfig];
+      const obj = {...arr[indexError]};
+      obj.error = true;
+      arr[indexError] = obj;
+      setQuoteConfig(arr);
+    }
   };
-
-
-  const columns = [
-    {
-      title: (
-        <Typography
-          name="Body 4/Medium"
-          className="dragHandler"
-          color={token?.colorPrimaryText}
-        >
-          Distributer
-        </Typography>
-      ),
-      dataIndex: 'distributer',
-      key: 'distributer',
-      width: 187,
-      render: (text: string, record: any, index: number) => (
-        <OsDistributorSelect
-          name={`distributor_${index}`}
-          distributorValue={record?.distributor_id}
-          isAddNewDistributor
-          height={38}
-          isRequired
-          form={form}
-          onChange={(value: any) => {
-            form.resetFields([`oem_${index}`]);
-            setQuoteConfig((prev: any) =>
-              prev.map((prevItem: any, prevIndex: number) => {
-                if (prevIndex === index) {
-                  return {
-                    ...prevItem,
-                    distributor_id: value,
-                  };
-                }
-                return prevItem;
-              }),
-            );
-          }}
-        />
-      ),
-    },
-    {
-      title: (
-        <Typography
-          name="Body 4/Medium"
-          className="dragHandler"
-          color={token?.colorPrimaryText}
-        >
-          OEM
-        </Typography>
-      ),
-      dataIndex: 'oem',
-      key: 'oem',
-      width: 130,
-      render: (text: string, record: any, index: number) => (
-        <OsOemSelect
-          name={`oem_${index}`}
-          oemValue={record?.oem_id}
-          isAddNewOem
-          isRequired
-          form={form}
-          onChange={(value: any) => {
-            setQuoteConfig((prev: any) =>
-              prev.map((prevItem: any, prevIndex: any) => {
-                if (prevIndex === index) {
-                  const obj = {
-                    ...prevItem,
-                    oem_id: value,
-                  };
-
-                  return obj;
-                }
-                return prevItem;
-              }),
-            );
-          }}
-        />
-      ),
-    },
-    {
-      title: (
-        <Typography
-          name="Body 4/Medium"
-          className="dragHandler"
-          color={token?.colorPrimaryText}
-        >
-          Model ID
-        </Typography>
-      ),
-      dataIndex: 'model_id',
-      key: 'model_id',
-      width: 187,
-      render: (text: string, record: any, index: number) => (
-        <OsInput
-          name={`model_${index}`}
-          placeholder="Write here"
-          style={{height: '38px'}}
-          value={text}
-          onChange={(e) => {
-            setQuoteConfig((prev: any) =>
-              prev.map((prevItem: any, prevIndex: number) => {
-                if (prevIndex === index) {
-                  return {
-                    ...prevItem,
-                    model_id: e?.target?.value,
-                  };
-                }
-                return prevItem;
-              }),
-            );
-          }}
-        />
-      ),
-    },
-    {
-      title: ' ',
-      dataIndex: 'actions',
-      key: 'actions',
-      width: 54,
-      render: (text: string, record: any, index: number) => (
-        <Space size={18}>
-          <TrashIcon
-            height={24}
-            width={24}
-            color={token.colorError}
-            style={{cursor: 'pointer'}}
-            onClick={() => {
-              setQuoteConfig((prev: any) =>
-                prev.filter(
-                  (prevItem: any, prevIndex: number) => prevIndex !== index,
-                ),
-              );
-            }}
-          />
-        </Space>
-      ),
-    },
-  ];
 
   const locale = {
     emptyText: (
@@ -206,7 +92,7 @@ const AllQuote: React.FC = () => {
     // setDeleteIds([]);
     // setShowModalDelete(false);
   };
-
+  const quoteConfigurationColumns = getColumns(token, setQuoteConfig, form);
   return (
     <>
       {contextHolder}
@@ -245,7 +131,7 @@ const AllQuote: React.FC = () => {
         >
           <Form layout="vertical" form={form}>
             <OsTable
-              columns={columns}
+              columns={quoteConfigurationColumns}
               dataSource={quoteConfig}
               scroll
               loading={loading}
@@ -261,8 +147,8 @@ const AllQuote: React.FC = () => {
             clickHandler={() => {
               const arr: any = [...quoteConfig];
               arr.push({
-                distributor_id: '',
-                oem_id: '',
+                distributor_id: null,
+                oem_id: null,
                 model_id: '',
               });
               setQuoteConfig(arr);
