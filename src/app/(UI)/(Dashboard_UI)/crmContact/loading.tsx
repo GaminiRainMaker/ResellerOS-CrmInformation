6b6be1c2c8474bda49e6f69fe2ed5a78c86bc1f3
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2024-03-18 12:22:18 +0530

    add skeleton for all are the pages

---
+++ src/app/(UI)/(Dashboard_UI)/crmContact/loading.tsx
@@ -0,0 +1,5 @@
+import PageSkeleton from '@/app/components/common/os-skeletons/PageSkeleton';
+
+export default function Loading() {
+  return <PageSkeleton />;
+}
--- src/app/components/common/os-skeletons/PageSkeleton.tsx
+++ src/app/components/common/os-skeletons/PageSkeleton.tsx
@@ -10,9 +10,14 @@ import BlankDivSkeleton from './BlankDivSkeleton';
 const PageSkeleton: FC = () => (
   <Space size={24} direction="vertical" style={{width: '100%'}}>
     <AnalyticCardSkeleton />{' '}
-    <Row justify="space-between">
+    <Row justify="space-between" align="middle">
       <Col span={12}>
-        <Skeleton paragraph={{rows: 0}} active />
+        <Skeleton.Button
+          shape="default"
+          size="large"
+          active
+          style={{height: '35px', width: '250px', borderRadius: '8px'}}
+        />
       </Col>
       <Col>
         <Space size={8}>
@@ -20,20 +25,20 @@ const PageSkeleton: FC = () => (
             shape="default"
             size="large"
             active
-            style={{height: '48px', width: '150px'}}
+            style={{height: '48px', width: '150px', borderRadius: '8px'}}
           />
           <Skeleton.Avatar
             active
             size="default"
             shape="square"
-            style={{height: '48px', width: '48px'}}
+            style={{height: '48px', width: '48px', borderRadius: '8px'}}
           />
         </Space>
       </Col>
     </Row>
     <BlankDivSkeleton
       height="calc(100vh - 300px)"
-      width="calc(100vw - 150px)"
+      width="calc(100vw - 200px)"
     />
   </Space>
 );
