1c7d2b03f201cf43e97609241621808b61b572ea
GaminiRainMaker <118426903+GaminiRainMaker@users.noreply.github.com>
2024-05-31 16:46:50 +0530

    added fixes

--- src/app/(UI)/(Dashboard_UI)/generateQuote/AddDocument.tsx
+++ src/app/(UI)/(Dashboard_UI)/generateQuote/AddDocument.tsx
@@ -6,17 +6,23 @@ import CommonSelect from '@/app/components/common/os-select';
 import {OSDraggerStyle} from '@/app/components/common/os-upload/styled-components';
 import Typography from '@/app/components/common/typography';
 import {FolderArrowDownIcon} from '@heroicons/react/24/outline';
-import {Form} from 'antd';
+import * as XLSX from 'xlsx';
+import {Button, Divider, Form, Upload} from 'antd';
 import {useRouter} from 'next/navigation';
-import {FC, useEffect} from 'react';
+import {FC, useEffect, useState} from 'react';
 import {queryAllDocuments} from '../../../../../redux/actions/formstack';
 import {getAllGeneralSetting} from '../../../../../redux/actions/generalSetting';
 import {useAppDispatch, useAppSelector} from '../../../../../redux/hook';
+import OsInput from '@/app/components/common/os-input';
+import {formatStatus} from '@/app/utils/CONSTANTS';
+import OsButton from '@/app/components/common/os-button';
+import {UploadOutlined} from '@ant-design/icons';
 
 const AddDocument: FC<any> = ({form, setShowDocumentModalButton}) => {
   const [token] = useThemeToken();
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const [extractedStrings, setExtractedStrings] = useState([]);
   const {data: FormstackData, loading: FormstackLoading} = useAppSelector(
     (state) => state.formstack,
   );
@@ -49,75 +55,216 @@ const AddDocument: FC<any> = ({form, setShowDocumentModalButton}) => {
       ),
     }));
 
-    
   const onFinish = () => {
     const values = form.getFieldsValue();
     console.log('onFinish', values);
   };
 
+  const handleFileUpload = (uploadedData: any) => {
+    if (!uploadedData.file) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const data = new Uint8Array(e.target.result);
+      try {
+        const workbook = XLSX.read(data, {type: 'array'});
+        processWorkbook(workbook);
+      } catch (error) {
+        console.log(
+          'Error reading the file. Please make sure the file is a valid Excel file.',
+        );
+      }
+    };
+    reader.readAsArrayBuffer(uploadedData.file);
+  };
+
+  const processWorkbook = (workbook: any) => {
+    const extracted: any = [];
+    const regex = /\{([^}]+)\}/g;
+
+    workbook.SheetNames.forEach((sheetName: any) => {
+      const sheet = workbook.Sheets[sheetName];
+      const csvData = XLSX.utils.sheet_to_csv(sheet);
+      const lines = csvData.split(/\r\n|\n/);
+
+      lines.forEach((line) => {
+        let match;
+        while (
+          (match = regex.exec(line)) !== null &&
+          !extracted?.includes(
+            match[1].replace('$', '').split(':')[0].split('|')[0],
+          )
+        ) {
+          extracted.push(match[1].replace('$', '').split(':')[0].split('|')[0]);
+        }
+      });
+    });
+
+    setExtractedStrings(extracted);
+  };
+
   return (
     <GlobalLoader loading={FormstackLoading || GeneralSettingLoading}>
       <br />
-      {FormstackDataOptions ? (
-        <Form
-          layout="vertical"
-          requiredMark={false}
-          form={form}
-          onFinish={onFinish}
-        >
-          {setShowDocumentModalButton(true)}
-          <Row gutter={[16, 24]} justify="space-between">
-            <Col span={24}>
-              <SelectFormItem
-                label={<Typography name="Body 4/Medium">Document</Typography>}
-                name="document_id"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Document is required!',
-                  },
-                ]}
-              >
-                <CommonSelect
-                  style={{width: '100%'}}
-                  placeholder="Select Document"
-                  allowClear
-                  options={FormstackDataOptions}
-                />
-              </SelectFormItem>
-            </Col>
 
-            <Col span={24}>
-              <OSDraggerStyle showUploadList={false} multiple>
-                <FolderArrowDownIcon
-                  width={24}
-                  color={token?.colorInfoBorder}
+      {!FormstackDataOptions ? (
+        <>
+          {extractedStrings?.length > 0 ? (
+            <div style={{height: '50vh', overflow: 'auto'}}>
+              <Row
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  padding: '20px',
+                }}
+              >
+                <Col>
+                  <Row style={{marginTop: '6px'}}>
+                    {' '}
+                    <Typography
+                      style={{marginLeft: '10px'}}
+                      align="center"
+                      name="Body 3/Medium"
+                    >
+                      Your File Header
+                    </Typography>
+                  </Row>
+                  <Divider />
+                  {extractedStrings?.map((item: any) => (
+                    <Row style={{marginTop: '6px'}}>
+                      <OsInput disabled value={formatStatus(item)} />
+                    </Row>
+                  ))}
+                </Col>
+
+                <Col>
+                  <Row style={{marginTop: '6px'}}>
+                    {' '}
+                    <Typography
+                      style={{marginLeft: '10px'}}
+                      align="center"
+                      name="Body 3/Medium"
+                    >
+                      Quote Header
+                    </Typography>
+                  </Row>
+                  <Divider />
+                  {extractedStrings?.map((item: any, indexOfCol: number) => (
+                    <Row style={{marginTop: '6px'}}>
+                      <CommonSelect
+                        onChange={(e) => {
+                          // syncTableToLineItems(item, e, indexOfCol);
+                        }}
+                        allowClear
+                        // onClear={() => handleChange(item?.newVal)}
+                        defaultValue={item?.newVal?.toString()?.toUpperCase()}
+                        style={{width: '250px'}}
+                        options={[]}
+                      />
+                    </Row>
+                  ))}
+                </Col>
+              </Row>
+              <Row
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                <OsButton
+                  text="Sync And Save"
+                  style={{
+                    width: '100%',
+                  }}
+                  buttontype="PRIMARY"
+                  // clickHandler={syncTableDataNew}
                 />
-                <Typography
-                  name="Body 4/Medium"
-                  color={token?.colorPrimaryText}
-                  as="div"
-                >
-                  <Typography
-                    name="Body 4/Medium"
-                    style={{textDecoration: 'underline', cursor: 'pointer'}}
-                    color={token?.colorPrimary}
-                    hoverOnText
-                  >
-                    Click to Upload
-                  </Typography>{' '}
-                  or Drag and Drop
-                </Typography>
-                <Typography
-                  name="Body 4/Medium"
-                  color={token?.colorPrimaryText}
-                >
-                  XLS, PDF.
-                </Typography>
-              </OSDraggerStyle>
-            </Col>
-          </Row>
-        </Form>
+              </Row>
+            </div>
+          ) : (
+            <>
+              <Form
+                layout="vertical"
+                requiredMark={false}
+                form={form}
+                onFinish={onFinish}
+              >
+                {setShowDocumentModalButton(true)}
+                <Row gutter={[16, 24]} justify="space-between">
+                  <Col span={24}>
+                    <SelectFormItem
+                      label={
+                        <Typography name="Body 4/Medium">Document</Typography>
+                      }
+                      name="document_id"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Document is required!',
+                        },
+                      ]}
+                    >
+                      <CommonSelect
+                        style={{width: '100%'}}
+                        placeholder="Select Document"
+                        allowClear
+                        options={FormstackDataOptions}
+                      />
+                    </SelectFormItem>
+                  </Col>
+
+                  <Col span={24}>
+                    <Upload
+                      // accept=".csv"
+                      beforeUpload={() => false} // Prevent automatic upload
+                      onChange={handleFileUpload}
+                    >
+                      <Button icon={<UploadOutlined />}>Upload CSV</Button>
+                    </Upload>
+                    <OSDraggerStyle
+                      showUploadList={false}
+                      multiple
+                      onChange={handleFileUpload}
+                      disabled
+                    >
+                      <FolderArrowDownIcon
+                        width={24}
+                        color={token?.colorInfoBorder}
+                      />
+                      <Typography
+                        name="Body 4/Medium"
+                        color={token?.colorPrimaryText}
+                        as="div"
+                      >
+                        <Typography
+                          name="Body 4/Medium"
+                          style={{
+                            textDecoration: 'underline',
+                            cursor: 'pointer',
+                          }}
+                          color={token?.colorPrimary}
+                          hoverOnText
+                        >
+                          Click to Upload
+                        </Typography>{' '}
+                        or Drag and Drop
+                      </Typography>
+                      <Typography
+                        name="Body 4/Medium"
+                        color={token?.colorPrimaryText}
+                      >
+                        XLS, PDF.
+                      </Typography>
+                    </OSDraggerStyle>
+                  </Col>
+                </Row>
+              </Form>
+            </>
+          )}
+        </>
       ) : (
         <>
           {setShowDocumentModalButton(false)}
