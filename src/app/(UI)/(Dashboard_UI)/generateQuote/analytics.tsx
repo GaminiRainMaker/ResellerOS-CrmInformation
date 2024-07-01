0799b596b2e9270039a62062d4bb8e1b00dc97a6
NamanRainmaker <nagrawal@rainmakercloud.com>
2024-07-01 15:42:24 +0530

    Update analytics.tsx

--- src/app/(UI)/(Dashboard_UI)/generateQuote/analytics.tsx
+++ src/app/(UI)/(Dashboard_UI)/generateQuote/analytics.tsx
@@ -69,16 +69,16 @@ const GenerateQuoteAnalytics: FC<any> = () => {
             exitPrice += item.exit_price;
           }
         }
-        // if (
-        //   item?.adjusted_price !== undefined &&
-        //   item?.quantity !== undefined
-        // ) {
-        //   let temp: any;
-        //   temp =
-        //     Number(item?.adjusted_price) *
-        //     (item?.quantity ? Number(item?.quantity) : 1);
-        //   adjustedPrice += temp;
-        // }
+        if (
+          item?.adjusted_price !== undefined &&
+          item?.quantity !== undefined
+        ) {
+          let temp: any;
+          temp =
+            Number(item?.adjusted_price) *
+            (item?.quantity ? Number(item?.quantity) : 1);
+          adjustedPrice += temp;
+        }
       });
     }
     const totalGrossProfit = grossProfit + bundleGrossProfit;
@@ -115,7 +115,7 @@ const GenerateQuoteAnalytics: FC<any> = () => {
     },
     {
       key: 3,
-      primary: `$${abbreviate(totalValues?.ExitPrice ?? 0)}`,
+      primary: `$${abbreviate(totalValues?.AdjustedPrice ?? 0)}`,
       secondry: 'Total Cost',
       icon: <CurrencyDollarIcon width={24} color={token?.colorLink} />,
       iconBg: token?.colorLinkActive,
