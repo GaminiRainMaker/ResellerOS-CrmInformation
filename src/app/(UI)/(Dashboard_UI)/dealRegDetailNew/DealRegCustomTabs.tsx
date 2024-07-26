4c391481a565e80b8a98533901f1f7927449f846
NamanRainmaker <nagrawal@rainmakercloud.com>
2024-07-26 12:28:14 +0530

    Update DealRegCustomTabs.tsx

--- src/app/(UI)/(Dashboard_UI)/dealRegDetailNew/DealRegCustomTabs.tsx
+++ src/app/(UI)/(Dashboard_UI)/dealRegDetailNew/DealRegCustomTabs.tsx
@@ -11,19 +11,18 @@ import CustomProgress from '@/app/components/common/os-progress/DealregProgressB
 import Typography from '@/app/components/common/typography';
 import {formatStatus} from '@/app/utils/CONSTANTS';
 import {tabBarPercentageCalculations} from '@/app/utils/base';
-import {useEffect, useMemo, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {queryAttributeField} from '../../../../../redux/actions/attributeField';
 import {useAppDispatch, useAppSelector} from '../../../../../redux/hook';
 import {setDealReg} from '../../../../../redux/slices/dealReg';
 import DealRegDetailForm from './DealRegDetailForm';
 
-const DealRegCustomTabs: React.FC<any> = ({
-  form,
-}) => {
+const DealRegCustomTabs: React.FC<any> = ({form}) => {
   const dispatch = useAppDispatch();
   const [token] = useThemeToken();
   const {data: DealRegData} = useAppSelector((state) => state.dealReg);
   const [activeKey, setActiveKey] = useState<number>();
+  const [tabItems, setTabItems] = useState([]);
   const {data: AttributeFieldData} = useAppSelector(
     (state) => state.attributeField,
   );
@@ -41,69 +40,80 @@ const DealRegCustomTabs: React.FC<any> = ({
     setActiveKey(key);
   };
 
-  const tabItems = useMemo(() => {
-    if (!DealRegData) return [];
-    return DealRegData?.map((element: any) => {
-      const {partner_program_id, Partner, PartnerProgram} = element;
-      const isActive = activeKey?.toString() === partner_program_id?.toString();
+  useEffect(() => {
+    if (!DealRegData) {
+      setTabItems([]);
+      return;
+    }
 
-      const tabPercentage = tabBarPercentageCalculations(
-        element?.PartnerProgram?.form_data,
-        AttributeFieldData,
-        element?.unique_form_data,
-        element?.common_form_data,
-      );
-      const headerStyle = {
-        background: isActive ? token.colorInfo : token.colorInfoBg,
-      };
-      const textColor = isActive
-        ? token.colorBgContainer
-        : token?.colorTextDisabled;
-      return {
-        key: partner_program_id,
-        label: (
-          <Row
-            key={partner_program_id}
-            gutter={[0, 10]}
-            style={{width: 'fit-content', margin: '24px 0px'}}
-          >
-            <DealRegCustomTabHeaderStyle
-              token={token}
-              style={headerStyle}
-              onClick={() => {
-                dispatch(setDealReg(element));
-              }}
+    const newTabItems =
+      DealRegData &&
+      DealRegData?.map((element: any) => {
+        const {partner_program_id, Partner, PartnerProgram} = element;
+        const isActive =
+          activeKey?.toString() === partner_program_id?.toString();
+
+        const tabPercentage = tabBarPercentageCalculations(
+          element?.PartnerProgram?.form_data,
+          AttributeFieldData,
+          element?.unique_form_data,
+          element?.common_form_data,
+        );
+
+        const headerStyle = {
+          background: isActive ? token.colorInfo : token.colorInfoBg,
+        };
+
+        const textColor = isActive
+          ? token.colorBgContainer
+          : token?.colorTextDisabled;
+
+        return {
+          key: partner_program_id,
+          label: (
+            <Row
+              key={partner_program_id}
+              gutter={[0, 10]}
+              style={{width: 'fit-content', margin: '24px 0px'}}
             >
-              <Space>
-                <CustomProgress
-                  isActive={isActive}
-                  token={token}
-                  percent={tabPercentage}
-                />
-                <Typography
-                  style={{color: textColor}}
-                  cursor="pointer"
-                  name="Button 1"
-                >
-                  {`${formatStatus(Partner?.partner)} - ${formatStatus(PartnerProgram?.partner_program)}`}
-                </Typography>
-              </Space>
-            </DealRegCustomTabHeaderStyle>
-          </Row>
-        ),
+              <DealRegCustomTabHeaderStyle
+                token={token}
+                style={headerStyle}
+                onClick={() => {
+                  dispatch(setDealReg(element));
+                }}
+              >
+                <Space>
+                  <CustomProgress
+                    isActive={isActive}
+                    token={token}
+                    percent={tabPercentage}
+                  />
+                  <Typography
+                    style={{color: textColor}}
+                    cursor="pointer"
+                    name="Button 1"
+                  >
+                    {`${formatStatus(Partner?.partner)} - ${formatStatus(PartnerProgram?.partner_program)}`}
+                  </Typography>
+                </Space>
+              </DealRegCustomTabHeaderStyle>
+            </Row>
+          ),
+          children: (
+            <div key={partner_program_id}>
+              <DealRegDetailForm
+                data={element}
+                activeKey={activeKey}
+                form={form}
+              />
+            </div>
+          ),
+        };
+      });
 
-        children: (
-          <div key={partner_program_id}>
-            <DealRegDetailForm
-              data={element}
-              activeKey={activeKey}
-              form={form}
-            />
-          </div>
-        ),
-      };
-    });
-  }, [DealRegData, activeKey]);
+    setTabItems(newTabItems);
+  }, [DealRegData, activeKey, AttributeFieldData, token, dispatch, form]);
 
   return (
     <>
@@ -118,3 +128,67 @@ const DealRegCustomTabs: React.FC<any> = ({
 };
 
 export default DealRegCustomTabs;
+
+// const tabItems = useMemo(() => {
+//   if (!DealRegData) return [];
+//   return DealRegData?.map((element: any) => {
+//     const {partner_program_id, Partner, PartnerProgram} = element;
+//     const isActive = activeKey?.toString() === partner_program_id?.toString();
+
+//     const tabPercentage = tabBarPercentageCalculations(
+//       element?.PartnerProgram?.form_data,
+//       AttributeFieldData,
+//       element?.unique_form_data,
+//       element?.common_form_data,
+//     );
+//     const headerStyle = {
+//       background: isActive ? token.colorInfo : token.colorInfoBg,
+//     };
+//     const textColor = isActive
+//       ? token.colorBgContainer
+//       : token?.colorTextDisabled;
+//     return {
+//       key: partner_program_id,
+//       label: (
+//         <Row
+//           key={partner_program_id}
+//           gutter={[0, 10]}
+//           style={{width: 'fit-content', margin: '24px 0px'}}
+//         >
+//           <DealRegCustomTabHeaderStyle
+//             token={token}
+//             style={headerStyle}
+//             onClick={() => {
+//               dispatch(setDealReg(element));
+//             }}
+//           >
+//             <Space>
+//               <CustomProgress
+//                 isActive={isActive}
+//                 token={token}
+//                 percent={tabPercentage}
+//               />
+//               <Typography
+//                 style={{color: textColor}}
+//                 cursor="pointer"
+//                 name="Button 1"
+//               >
+//                 {`${formatStatus(Partner?.partner)} - ${formatStatus(PartnerProgram?.partner_program)}`}
+//               </Typography>
+//             </Space>
+//           </DealRegCustomTabHeaderStyle>
+//         </Row>
+//       ),
+
+//       children: (
+//         <div key={partner_program_id}>
+//           <DealRegDetailForm
+//             data={element}
+//             activeKey={activeKey}
+//             form={form}
+//           />
+//         </div>
+//       ),
+//     };
+//   });
+// }, [DealRegData, activeKey]);
