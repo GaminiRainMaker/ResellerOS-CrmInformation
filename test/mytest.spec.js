1421a48b3051f67d696734625a1fb152824b0b4b
Gamini Jindal <gjindal@rainmakercloud.com>
2024-08-13 13:42:58 +0530

    changes done for test case

--- test/mytest.spec.js
+++ test/mytest.spec.js
@@ -13,18 +13,44 @@ test('test', async ({page}) => {
   await page.locator('button:has-text("Add Opportunity")').nth(0).click();
   await page.getByLabel('Customer').click();
 
-  const selectedOption = page
-    .getByTestId('custom_select')
-    .locator('option[selected="selected"]');
+  // Function to repeatedly check if the select element has the expected value
+  async function waitForSelectValue(selector, timeout = 30000) {
+    const start = Date.now();
+
+    while (Date.now() - start < timeout) {
+      const value = await page.getByTestId(selector).innerText();
+      console.log('valuevalue', value);
+
+      if (value && value !== 'Select') {
+        return true;
+      }
+      // Wait for a short period before checking again
+      await page.waitForTimeout(500); // Adjust this value as needed
+    }
+
+    throw new Error(
+      `Timeout: Select element did not get the value within ${timeout / 1000} seconds.`,
+    );
+  }
+
+  await waitForSelectValue('custom_select');
+
+  const selectedOption = await page.getByTestId('custom_select').innerText();
+  // expect(selectedOption).not.toBeEmpty();
   console.log('selectedOption', selectedOption);
 
-  if (selectedOption) {
-    await page.getByPlaceholder('Enter Text').fill('Opportunity Demo');
-    await page.getByPlaceholder('Enter Amount').fill('2,352,4563');
-    await page.getByLabel('Stages').click();
-    await page.getByText('Develop').nth(3).click();
-    // await page.getByRole('button', {name: 'Save'}).click();
-  }
+  // await page.waitForFunction(async () => {
+  //   const value = await page
+  //     .getByTestId('custom_select')
+  //     .evaluate('el => el.options[el.selectedIndex].text');
+  //   return value ? true : false;
+  // });
+
+  await page.getByPlaceholder('Enter Text').fill('Opportunity Demo');
+  await page.getByPlaceholder('Enter Amount').fill('2,352,4563');
+  await page.getByLabel('Stages').click();
+  await page.getByText('Develop').nth(3).click();
+  // await page.getByRole('button', {name: 'Save'}).click();
 
   await page.pause();
 });
