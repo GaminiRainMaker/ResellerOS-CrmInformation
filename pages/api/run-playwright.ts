565b9d32fca4946a688a4ac7ec388146e04601ef
Gamini Jindal <gjindal@rainmakercloud.com>
2024-08-23 13:02:07 +0530

    chnaged script and removed unwanted dependencies

--- pages/api/run-playwright.ts
+++ pages/api/run-playwright.ts
@@ -1,8 +1,5 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-import {run} from 'jest-cli';
-import {promises as fs} from 'fs';
-import path from 'path';
-import { chromium } from 'playwright';
+import {chromium} from 'playwright';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,39 +7,40 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     const data = req.body.data;
+    const script = JSON.parse(data?.script[0]);
 
     // Log the incoming request data for debugging
-    console.log('Received request data:', data);
 
     // Create a temporary config file to pass data
     // const configFilePath = path.join(process.cwd(), 'jest.config.js');
 
-    const configContent = `
-      module.exports = {
-        testRegex: 'tests/sampleTest.spec.js',
-        testEnvironmentOptions: {
-          data: ${data},
-        },
-      };
-    `;
-
+    const executeScript = new Function(
+      'page',
+      `
+      return (async () => {
+          ${script}
+      })();
+  `,
+    );
     try {
       // Launch Chromium
       const browser = await chromium.launch({headless: false});
       const context = await browser.newContext();
       const page = await context.newPage();
+      console.log(data?.script, 'sdfbsdbf');
 
       // Dynamically execute the script
-      const executeScript = new Function('page', data?.script);
       await executeScript(page);
 
       await browser.close();
 
-      res.status(200).json({ message: 'Script executed successfully' });
-  } catch (error: any) {
+      res.status(200).json({message: 'Script executed successfully'});
+    } catch (error: any) {
       console.error(error);
-      res.status(500).json({ message: 'Error executing script', error: error.message });
-  }
+      res
+        .status(500)
+        .json({message: 'Error executing script', error: error.message});
+    }
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
