f0092e162f89ecbd6357733fd415e089d70a1cce
GaminiRainMaker <gjindal@rainmakercloud.com>
2024-10-21 16:59:23 +0530

    added  changes for azure AI

--- src/app/(UI)/(Dashboard_UI)/generateQuote/allTabs/ReviewQuotes.tsx
+++ src/app/(UI)/(Dashboard_UI)/generateQuote/allTabs/ReviewQuotes.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Col, Row } from '@/app/components/common/antd/Grid';
-import { Space } from '@/app/components/common/antd/Space';
+import {Col, Row} from '@/app/components/common/antd/Grid';
+import {Space} from '@/app/components/common/antd/Space';
 import useAbbreviationHook from '@/app/components/common/hooks/useAbbreviationHook';
 import useThemeToken from '@/app/components/common/hooks/useThemeToken';
 import OsCollapse from '@/app/components/common/os-collapse';
@@ -10,13 +10,13 @@ import EmptyContainer from '@/app/components/common/os-empty-container';
 import OsModal from '@/app/components/common/os-modal';
 import RaiseConcern from '@/app/components/common/os-raise-concern';
 import OsTableWithOutDrag from '@/app/components/common/os-table/CustomTable';
-import { AvatarStyled } from '@/app/components/common/os-table/styled-components';
+import {AvatarStyled} from '@/app/components/common/os-table/styled-components';
 import Typography from '@/app/components/common/typography';
-import { updateTables, useRemoveDollarAndCommahook } from '@/app/utils/base';
-import { CheckIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Form, notification } from 'antd';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { FC, useEffect, useState } from 'react';
+import {updateTables, useRemoveDollarAndCommahook} from '@/app/utils/base';
+import {CheckIcon, XMarkIcon} from '@heroicons/react/24/outline';
+import {Form, notification} from 'antd';
+import {useRouter, useSearchParams} from 'next/navigation';
+import {FC, useEffect, useState} from 'react';
 import GreenCheckIcon from '../../../../../../public/assets/static/greenCheckIcon.svg';
 import RaiseConcernImg from '../../../../../../public/assets/static/raiseConcern.svg';
 import {
@@ -25,10 +25,10 @@ import {
   getQuoteFileCount,
   quoteFileVerification,
 } from '../../../../../../redux/actions/quoteFile';
-import { useAppDispatch, useAppSelector } from '../../../../../../redux/hook';
-import { setConcernQuoteLineItemData } from '../../../../../../redux/slices/quotelineitem';
-import { getProfitabilityByQuoteId } from '../../../../../../redux/actions/profitability';
-import { setProfitability } from '../../../../../../redux/slices/profitability';
+import {useAppDispatch, useAppSelector} from '../../../../../../redux/hook';
+import {setConcernQuoteLineItemData} from '../../../../../../redux/slices/quotelineitem';
+import {getProfitabilityByQuoteId} from '../../../../../../redux/actions/profitability';
+import {setProfitability} from '../../../../../../redux/slices/profitability';
 import {
   setQuoteFileDataCount,
   setQuoteFileUnverifiedById,
@@ -47,10 +47,10 @@ const ReviewQuotes: FC<any> = ({
   const searchParams = useSearchParams()!;
   const getQuoteID = searchParams.get('id');
   const isView = searchParams.get('isView');
-  const { abbreviate } = useAbbreviationHook(0);
+  const {abbreviate} = useAbbreviationHook(0);
   const [form] = Form.useForm();
-  const { userInformation } = useAppSelector((state) => state.user);
-  const { loading: quoteFileDataLoading, quoteFileUnverifiedById } =
+  const {userInformation} = useAppSelector((state) => state.user);
+  const {loading: quoteFileDataLoading, quoteFileUnverifiedById} =
     useAppSelector((state) => state.quoteFile);
   const [showRaiseConcernModal, setShowRaiseConcernModal] =
     useState<boolean>(false);
@@ -110,13 +110,17 @@ const ReviewQuotes: FC<any> = ({
           groupedData[name].QuoteLineItem.push(quoteLineItem);
           groupedData[name].totalAdjustedPrice += Number(
             useRemoveDollarAndCommahook(
-              quoteLineItem?.adjusted_price
+              quoteLineItem?.adjusted_price &&
+                typeof quoteLineItem?.adjusted_price === 'number'
                 ? quoteLineItem?.adjusted_price
                 : 0,
             ) *
-            useRemoveDollarAndCommahook(
-              quoteLineItem?.quantity ? quoteLineItem?.quantity : 0,
-            ),
+              useRemoveDollarAndCommahook(
+                quoteLineItem?.quantity &&
+                  typeof quoteLineItem?.quantity === 'number'
+                  ? quoteLineItem?.quantity
+                  : 0,
+              ),
           );
         });
       }
@@ -137,7 +141,7 @@ const ReviewQuotes: FC<any> = ({
               : items?.QuoteLineItem?.length < 30
                 ? 30
                 : items?.QuoteLineItem?.length < 31 ||
-                  items?.QuoteLineItem?.length < 51
+                    items?.QuoteLineItem?.length < 51
                   ? 50
                   : 100,
         total: 0,
@@ -184,7 +188,9 @@ const ReviewQuotes: FC<any> = ({
       key: 'quantity',
       sorter: (a: any, b: any) => a.quantity - b.quantity,
       render: (text: any, record: any) => (
-        <Typography name="Body 4/Medium">{text ?? 0}</Typography>
+        <Typography name="Body 4/Medium">
+          {typeof text === 'number' && text ? text : 0}
+        </Typography>
       ),
       width: 120,
     },
@@ -229,7 +235,7 @@ const ReviewQuotes: FC<any> = ({
             <CommonSelect
               disabled={true}
               allowClear
-              style={{ width: '200px', height: '36px' }}
+              style={{width: '200px', height: '36px'}}
               placeholder="Select"
               defaultValue={text ?? record?.Product?.product_family}
             />
@@ -294,7 +300,7 @@ const ReviewQuotes: FC<any> = ({
       setShowRaiseConcernModal(false);
       form?.resetFields();
     } else if (buttonType === 'fourth') {
-      await dispatch(quoteFileVerification({ id: fileData?.id }));
+      await dispatch(quoteFileVerification({id: fileData?.id}));
       await dispatch(getQuoteFileCount(Number(getQuoteID)));
       await dispatch(getQuoteFileByQuoteId(Number(getQuoteID)));
       setShowRaiseConcernModal(false);
@@ -343,7 +349,7 @@ const ReviewQuotes: FC<any> = ({
     }
   };
 
-
+  console.log('reviewQuotesDatareviewQuotesData', reviewQuotesData);
   return (
     <GlobalLoader loading={quoteFileDataLoading}>
       {contextHolder}
