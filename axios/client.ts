b5fd8b5a987660a136699cba76fd0ac53d0bcdc1
GaminiRainMaker <gjindal@rainmakercloud.com>
2024-10-21 19:49:07 +0530

    Update client.ts

--- axios/client.ts
+++ axios/client.ts
@@ -6,15 +6,15 @@ import {
 } from './interceptors/response.interceptor';
 
 const client = axios.create({
-  //Local
-  // baseURL: 'http://localhost:4000',
+  // Local
+  baseURL: 'http://localhost:4000',
 
   //Developement
-  // baseURL: 'https://3.218.195.191:4000',
+  // baseURL: 'https://api-dev.reselleros.com',
 
   //Production
-  baseURL: 'https://api.reselleros.com',
-  
+  // baseURL: 'https://api.reselleros.com',
+
   withCredentials: false,
 });
 
