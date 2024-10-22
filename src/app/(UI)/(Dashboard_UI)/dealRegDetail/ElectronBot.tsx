04ba4f9c5903460cba98073e93d5ff9fa52108a5
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2024-10-22 20:26:39 +0530

    Update ElectronBot.tsx

--- src/app/(UI)/(Dashboard_UI)/dealRegDetail/ElectronBot.tsx
+++ src/app/(UI)/(Dashboard_UI)/dealRegDetail/ElectronBot.tsx
@@ -52,7 +52,7 @@ const ElectronBot = () => {
       case 'windows':
         return 'https://reselller-os.s3.amazonaws.com/DealRegAI+Setup+0.1.0.exe';
       case 'mac':
-        return 'https://reselller-os.s3.amazonaws.com/DealRegAI-0.1.0.dmg.zip';
+        return 'https://reselller-os.s3.amazonaws.com/DealRegAI-0.1.0.dmg';
       case 'linux':
         return 'https://reselller-os.s3.amazonaws.com/MyApp-linux-x64.zip';
       default:
