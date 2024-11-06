0b34f6ac5896cab5ec15c8480d0b284b36f54bba
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2024-11-06 12:56:08 +0530

    Update client.ts

--- axios/client.ts
+++ axios/client.ts
@@ -7,13 +7,13 @@ import {
 
 const client = axios.create({
   //Local
-  // baseURL: 'http://localhost:4000',
+  baseURL: 'http://localhost:4000',
 
   //Developement
   // baseURL: 'https://api-dev.reselleros.com',
 
   //Production
-  baseURL: 'https://api.reselleros.com',
+  // baseURL: 'https://api.reselleros.com',
 
   withCredentials: false,
 });
