edb7ea7a9acb540c38048123b9443f81b36df526
GaminiRainMaker <gjindal@rainmakercloud.com>
2024-12-02 16:42:21 +0530

    Update response.interceptor.ts

--- axios/interceptors/response.interceptor.ts
+++ axios/interceptors/response.interceptor.ts
@@ -13,8 +13,8 @@ export const responseInterceptor = (config: AxiosResponse): AxiosResponse => {
 };
 export const errorResponseInterceptor = (error: AxiosError) => {
   if (error?.response?.status == 401) {
-    Cookies.remove('token');
-    window.location.href = '/' as string;
+    // Cookies.remove('token');
+    // window.location.href = '/' as string;
   }
   if (error?.response?.status == 400) {
     return Promise.resolve({
