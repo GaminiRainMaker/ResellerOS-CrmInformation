8a09019d0a2ea93f58f80ba2cf02a158bcf165ac
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2024-12-13 14:05:40 +0530

    add edit field functionality

--- src/app/(UI)/(Dashboard_UI)/userManagement/page.tsx
+++ src/app/(UI)/(Dashboard_UI)/userManagement/page.tsx
@@ -11,7 +11,11 @@ import OsModal from '@/app/components/common/os-modal';
 import CommonSelect from '@/app/components/common/os-select';
 import OsTable from '@/app/components/common/os-table';
 import Typography from '@/app/components/common/typography';
-import {EyeIcon, UsersIcon} from '@heroicons/react/24/outline';
+import {
+  EyeIcon,
+  PencilSquareIcon,
+  UsersIcon,
+} from '@heroicons/react/24/outline';
 import {Form, notification} from 'antd';
 import {Option} from 'antd/es/mentions';
 import {useRouter} from 'next/navigation';
@@ -25,17 +29,21 @@ import {
 import {
   createNewOrganization,
   getAdminUserOfAllOrganization,
+  updateUserById,
 } from '../../../../../redux/actions/user';
 import {useAppDispatch, useAppSelector} from '../../../../../redux/hook';
 import {setAllResellerRecord} from '../../../../../redux/slices/user';
 import AddNewOrganization from './AddNewOrganization';
 import AssignPartnerProgram from './AssignPartnerProgram';
+import OsDrawer from '@/app/components/common/os-drawer';
+import EditOrgDetail from './EditOrgDetail';
 
 const UserManagement = () => {
   const dispatch = useAppDispatch();
   const [token] = useThemeToken();
   const router = useRouter();
   const [form] = Form.useForm();
+  const [editOrgForm] = Form.useForm();
   const [newOrganizationFormData] = Form.useForm();
   const {data: userData, loading} = useAppSelector((state) => state.user);
   const {loading: AssignPartnerProgramLoading} = useAppSelector(
@@ -43,6 +51,8 @@ const UserManagement = () => {
   );
   const [showPartnerProgramAssignModal, setShowPartnerProgramAssignModal] =
     useState<boolean>(false);
+  const [showEditDrawer, setShowEditDrawer] = useState<boolean>(false);
+  const [editRecordData, setEditRecordData] = useState<any>();
 
   const [selectedRecordData, setSelectedRecordData] = useState<any>();
   const [showNewOrganizationModal, setShowNewOrganizationModal] =
@@ -151,7 +161,6 @@ const UserManagement = () => {
         return <Checkbox checked={text} />;
       },
     },
-
     {
       title: (
         <Typography name="Body 4/Medium" className="dragHandler">
@@ -160,7 +169,7 @@ const UserManagement = () => {
       ),
       dataIndex: 'actions',
       key: 'actions',
-      width: 101,
+      width: 250,
       render: (text: string, record: any) => (
         <Space size={18}>
           <EyeIcon
@@ -184,6 +193,22 @@ const UserManagement = () => {
               setShowPartnerProgramAssignModal(true);
               dispatch(setAllResellerRecord(record));
             }}
+          />
+          <PencilSquareIcon
+            height={24}
+            width={24}
+            onClick={() => {
+              setShowEditDrawer(true);
+              setEditRecordData(record);
+              editOrgForm.setFieldsValue({
+                org_id: record?.org_id,
+                organization: record?.organization,
+                email: record?.email,
+                user_name: record?.user_name,
+              });
+            }}
+            color={token.colorInfoBorder}
+            style={{cursor: 'pointer'}}
           />
         </Space>
       ),
@@ -395,6 +420,20 @@ const UserManagement = () => {
       newOrganizationFormData.resetFields();
     });
   };
+  const updateData = () => {
+    const orgData = editOrgForm.getFieldsValue();
+    let finalUpdateData = {
+      ...orgData,
+      id: editRecordData?.id,
+    };
+    dispatch(updateUserById(finalUpdateData))?.then((res) => {
+      if (res?.payload) {
+        dispatch(getAdminUserOfAllOrganization(searchQuery));
+        setShowEditDrawer(false);
+        editOrgForm?.resetFields();
+      }
+    });
+  };
 
   return (
     <>
@@ -569,6 +608,38 @@ const UserManagement = () => {
           newOrganizationFormData?.submit();
         }}
       />
+
+      <OsDrawer
+        title={
+          <Typography name="Body 1/Regular">
+            Update User Organization
+          </Typography>
+        }
+        placement="right"
+        onClose={() => {
+          setShowEditDrawer(false);
+          editOrgForm.resetFields();
+        }}
+        open={showEditDrawer}
+        width={450}
+        footer={
+          <Row style={{width: '100%', float: 'right'}}>
+            <OsButton
+              btnStyle={{width: '100%'}}
+              buttontype="PRIMARY"
+              text="Update Changes"
+              clickHandler={() => editOrgForm.submit()}
+            />
+          </Row>
+        }
+      >
+        <EditOrgDetail
+          onFinish={updateData}
+          form={editOrgForm}
+          drawer
+          editRecordData={editRecordData}
+        />
+      </OsDrawer>
     </>
   );
 };
