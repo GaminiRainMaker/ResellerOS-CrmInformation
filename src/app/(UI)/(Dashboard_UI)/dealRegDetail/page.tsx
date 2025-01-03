34552b3cffc7467a06b20d5abf551da4d32d544b
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2025-01-03 15:09:59 +0530

    add login script functionality

--- src/app/(UI)/(Dashboard_UI)/dealRegDetail/page.tsx
+++ src/app/(UI)/(Dashboard_UI)/dealRegDetail/page.tsx
@@ -54,6 +54,7 @@ const DealRegDetail = () => {
   const [showModal, setShowModal] = useState(false);
   const [showSubmitFormModal, setShowSubmitFormModal] = useState(false);
   const [electronBotModal, showElectronBotModal] = useState(false);
+  const [isSubmitLoginForm, showIsSubmitLoginForm] = useState(false);
   const searchParams = useSearchParams()!;
   const getOpportunityId = searchParams && searchParams.get('opportunityId');
   const [formData, setFormData] = useState<any>();
@@ -175,6 +176,8 @@ const DealRegDetail = () => {
           script: JSON.stringify(scriptEncryption),
         };
         const response = await dispatch(dealRegFormScript(desktopAppData));
+        if (response) showIsSubmitLoginForm(false);
+
         if (response && !isCanvas) {
           await dispatch(updateDealRegStatus(SubmitDealRegFormData)).then(
             (response: {payload: any}) => {
@@ -274,7 +277,9 @@ const DealRegDetail = () => {
           ? finalMainData?.credentials?.password
           : finalMainData.password,
         data: updatedData,
-        script: PartnerProgram?.script,
+        script: isSubmitLoginForm
+          ? PartnerProgram?.login_script
+          : PartnerProgram?.script,
         isLoginStep: PartnerProgram?.login_step,
       };
       const processScriptData = processScript(finalData);
@@ -282,6 +287,7 @@ const DealRegDetail = () => {
         return processScriptData;
       }
     } catch (error) {
+      showIsSubmitLoginForm(false);
       console.error('Error in createScript:', error);
       throw error; // Rethrow the error for the caller to handle
     }
@@ -358,7 +364,8 @@ const DealRegDetail = () => {
         footer={false}
       />
       <OsModal
-        loading={dealRegLoading}
+        loading={isSubmitLoginForm ? false : dealRegLoading}
+        thirdLoading={dealRegLoading}
         title="Submit DealReg Forms"
         bodyPadding={22}
         body={
@@ -374,7 +381,12 @@ const DealRegDetail = () => {
           setShowSubmitFormModal(false);
           submitDealRegForm?.resetFields();
         }}
-        primaryButtonText={'Submit'}
+        thirdButtonfunction={() => {
+          showIsSubmitLoginForm(true);
+          submitDealRegForm?.submit();
+        }}
+        primaryButtonText={'Submit Form'}
+        thirdButtonText="Submit Login Form"
       />
 
       <OsModal
