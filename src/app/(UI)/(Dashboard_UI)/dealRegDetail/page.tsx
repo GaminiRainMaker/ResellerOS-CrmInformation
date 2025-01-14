2045553ed743361ee1f87560a68735b386834a14
Gamini Jindal <gjindal@rainmakercloud.com>
2025-01-14 19:41:36 +0530

    consoles added

--- src/app/(UI)/(Dashboard_UI)/dealRegDetail/page.tsx
+++ src/app/(UI)/(Dashboard_UI)/dealRegDetail/page.tsx
@@ -320,7 +320,7 @@ const DealRegDetail = () => {
       let updatedData = newFormData
         ? dependentFieldProcess(template, newFormData)
         : null;
-
+      console.log(isSubmitLoginForm, 'isSubmitLoginFormisSubmitLoginForm');
       const finalData = {
         username: isSalesForce
           ? finalMainData?.credentials?.username
