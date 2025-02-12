34f4300934d5abbcb78cc6bbb946c03133d623d0
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2025-02-12 17:51:00 +0530

    add verifyEmail UI

--- src/app/(UI)/verifyEmail/page.tsx
+++ src/app/(UI)/verifyEmail/page.tsx
@@ -1,6 +1,17 @@
 'use client';
 
 import AuthLayout from '@/app/components/layouts/auth-layouts/AuthLayout';
+import eyeIcon from '../../../../public/assets/static/iconsax-svg/Svg/All/outline/eye.svg';
+
+const inputFields = [
+  {
+    name: 'password',
+    type: 'password',
+    label: 'Password',
+    placeholder: 'Minimum 8 characters',
+    icon: eyeIcon,
+  },
+];
 
 const VerifyEmail = () => {
   return (
@@ -9,6 +20,7 @@ const VerifyEmail = () => {
       description="Please click the button below to verify your email and complete your account setup."
       buttonText="Verify Email"
       alreadyAmember
+      inputFields={inputFields}
       // onFinish
       onClick=""
     />
