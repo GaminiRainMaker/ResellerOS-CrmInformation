358d688f06605fe9bdbe9ecabe655452e1fddc82
NamanRainmaker <150934286+NamanRainmaker@users.noreply.github.com>
2025-02-17 17:30:55 +0530

    update default pagination

--- src/app/(UI)/(Dashboard_UI)/crmOpportunity/page.tsx
+++ src/app/(UI)/(Dashboard_UI)/crmOpportunity/page.tsx
@@ -83,7 +83,7 @@ const CrmOpportunity: React.FC = () => {
   }>({
     opportunity: null,
     customer: null,
-    pageSize: 10,
+    pageSize: 5,
     pageNumber: 1,
   });
   const searchQuery = useDebounceHook(query, 500);
