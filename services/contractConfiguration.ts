57395dbbfbed6b4116e7f0889aa91adeb02dffb3
Gamini Jindal <gjindal@rainmakercloud.com>
2025-03-04 15:16:30 +0530

    remove unwanted services

--- services/contractConfiguration.ts
+++
@@ -1,9 +0,0 @@
-import {AxiosResponse} from 'axios';
-import {API} from './CONSTANTS';
-import {get, post} from './index';
-
-export const CONTRACT_CONFIGURATION_API = {
-  post: (data: any) => post(API.CONTRACT_CONFIGURATION.INDEX, data),
-  get: () =>
-    get(`${API.CONTRACT_CONFIGURATION.INDEX}`) as Promise<AxiosResponse<any>>,
-};
--- services/opportunityLineItem.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import {AxiosResponse} from 'axios';
-import {API} from './CONSTANTS';
-import {post} from './index';
-
-export const OPPORLINEITEM_API = {
-  // get: () => get(API.BUNDLE.INDEX) as Promise<AxiosResponse<[]>>,
-  post: (data: any) => post(API.OPPORLINEITEM.INDEX, data),
-};
